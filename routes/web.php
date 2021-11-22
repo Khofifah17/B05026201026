8c7867bf2d014ced2dbfb76f34767c540982ac48
Khofifah17 <5026201026@mahasiswa.integra.its.ac.id>
2021-11-22 15:57:27 +0700

    main

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,15 @@ Route::post('pajak','ViewController@hitungPajak');
 
 //route CRUD
 Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+Route::get('/mutasi','MutasiController@index');
+Route::get('/mutasi/tambah','MutasiController@tambah');
+Route::post('/mutasi/store','MutasiController@store');
+Route::get('/mutasi/edit/{id}','MutasiController@edit');
+Route::post('/mutasi/update','MutasiController@update');
+Route::get('/mutasi/hapus/{id}','MutasiController@hapus');
